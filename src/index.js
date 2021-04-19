5b1a17312fcc4cf1b30daa5d229af3e781a20cb2
pratikal1 <chandpratikal1@gmail.com>
2021-04-19 23:47:15 +0530

    connected to firebase/firestore

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDwu4BWmjdFeVRD3wrT4YTGkG5EFQTVDC0",
+  authDomain: "evernote-clone-3f1f1.firebaseapp.com",
+  projectId: "evernote-clone-3f1f1",
+  storageBucket: "evernote-clone-3f1f1.appspot.com",
+  messagingSenderId: "364841710683",
+  appId: "1:364841710683:web:c85b0fba8f5789c807bb7e"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export const projectFirestore = firebase.firestore();
 
 ReactDOM.render(
   <React.StrictMode>
